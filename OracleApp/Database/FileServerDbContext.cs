31798bb14aede1b65304e3ee9bb5c7eb85312914
meni-dev <menisaran15@gmail.com>
2024-08-07 08:59:15 +0530

    Add project files.

---
+++ OracleApp/Database/FileServerDbContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+using OracleApp.Database.Models;
+
+namespace OracleApp.Database
+{
+    public class FileServerDbContext : DbContext
+    {
+        public FileServerDbContext(DbContextOptions<FileServerDbContext> options) : base(options)
+        {
+        }
+
+        public DbSet<FileInformation> FileInfo { get; set; }
+    }
+}
---
+++ OracleApp/Database/Models/FileInfo.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace OracleApp.Database.Models
+{
+    public class FileInformation
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public Guid Id { get; set; }
+
+        public string FilePath { get; set; }
+    }
+}
